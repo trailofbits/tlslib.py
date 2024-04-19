2ce23b87e31c7f6cbdb6438e46f99003e7635ca5
Joop van de Pol <joop.vandepol@trailofbits.com>
2024-04-19 18:50:47 +0200

    In-memory channel implementation from PEP 543

--- src/tlslib/stdlib.py
+++ src/tlslib/stdlib.py
@@ -8,7 +8,7 @@ import ssl
 import tempfile
 import typing
 import weakref
-from collections.abc import Sequence
+from collections.abc import Buffer, Sequence
 from contextlib import contextmanager
 from pathlib import Path
 
@@ -60,6 +60,7 @@ def _error_converter(
     try:
         yield
     except ignore_filter:
+        print("LOL")
         pass
     except ssl.SSLWantReadError:
         raise WantReadError("Must read data") from None
@@ -527,6 +528,310 @@ class OpenSSLTLSSocket:
             return TLSVersion(ossl_version)
 
 
+class OpenSSLTLSBuffer:
+    """A TLSBuffer implementation based on OpenSSL"""
+
+    __slots__ = (
+        "_ciphertext_buffer",
+        "_in_bio",
+        "_object",
+        "_out_bio",
+        "_parent_context",
+        "_ssl_context",
+    )
+
+    _ciphertext_buffer: bytearray
+    _in_bio: ssl.MemoryBIO
+    _object: ssl.SSLObject
+    _out_bio: ssl.MemoryBIO
+    _parent_context: OpenSSLClientContext | OpenSSLServerContext
+    _ssl_context: _SSLContext
+
+    def __init__(self, *args: tuple, **kwargs: tuple) -> None:
+        """OpenTLSBuffers should not be constructed by the user.
+        Instead, the ClientContext.create_buffer() and
+        ServerContext.create_buffer() use the _create() method."""
+        msg = (
+            f"{self.__class__.__name__} does not have a public constructor. "
+            "Instances are returned by ClientContext.create_buffer() \
+                or ServerContext.create_buffer()."
+        )
+        raise TypeError(
+            msg,
+        )
+
+    @classmethod
+    def _create(
+        cls,
+        server_hostname: str | None,
+        parent_context: OpenSSLClientContext | OpenSSLServerContext,
+        server_side: bool,
+        ssl_context: _SSLContext,
+    ) -> OpenSSLTLSBuffer:
+        self = cls.__new__(cls)
+        self._parent_context = parent_context
+        self._ssl_context = ssl_context
+
+        # We need this extra buffer to implement the peek/consume API, which
+        # the MemoryBIO object does not allow.
+        self._ciphertext_buffer = bytearray()
+
+        # Set up the SSLObject we're going to back this with.
+        self._in_bio = ssl.MemoryBIO()
+        self._out_bio = ssl.MemoryBIO()
+
+        if server_side is True:
+            with _error_converter():
+                self._object = ssl_context.wrap_bio(
+                    self._in_bio, self._out_bio, server_side=True, server_hostname=None
+                )
+        else:
+            with _error_converter():
+                self._object = ssl_context.wrap_bio(
+                    self._in_bio, self._out_bio, server_side=False, server_hostname=server_hostname
+                )
+
+        return self
+
+    def read(self, amt: int) -> bytes:
+        """
+        Read up to ``amt`` bytes of data from the input buffer and return
+        the result as a ``bytes`` instance.
+
+        Once EOF is reached, all further calls to this method return the
+        empty byte string ``b''``.
+
+        May read "short": that is, fewer bytes may be returned than were
+        requested.
+
+        Raise ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read.
+
+        May raise ``RaggedEOF`` if the connection has been closed without a
+        graceful TLS shutdown. Whether this is an exception that should be
+        ignored or not is up to the specific application.
+
+        As at any time a re-negotiation is possible, a call to ``read()``
+        can also cause write operations.
+        """
+
+        try:
+            with _error_converter(ignore_filter=(ssl.SSLZeroReturnError,)):
+                return self._object.read(amt)
+        except ssl.SSLZeroReturnError:
+            print("You know it")
+            return b""
+
+    def readinto(self, buffer: Buffer, amt: int) -> int:
+        """
+        Read up to ``amt`` bytes of data from the input buffer into
+        ``buffer``, which must be an object that implements the buffer
+        protocol. Returns the number of bytes read.
+
+        Once EOF is reached, all further calls to this method return the
+        empty byte string ``b''``.
+
+        Raises ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read.
+
+        May read "short": that is, fewer bytes may be read than were
+        requested.
+
+        May raise ``RaggedEOF`` if the connection has been closed without a
+        graceful TLS shutdown. Whether this is an exception that should be
+        ignored or not is up to the specific application.
+
+        As at any time a re-negotiation is possible, a call to
+        ``readinto()`` can also cause write operations.
+        """
+
+        try:
+            with _error_converter(ignore_filter=(ssl.SSLZeroReturnError,)):
+                # MyPy does not recognize:
+                # - that the read function can return integers if a buffer is provided
+                # - that the buffer can be something other than a bytearray
+                return self._object.read(amt, buffer)  # type: ignore[return-value,arg-type]
+        except ssl.SSLZeroReturnError:
+            return 0
+
+    def write(self, buf: Buffer) -> int:
+        """
+        Write ``buf`` in encrypted form to the output buffer and return the
+        number of bytes written. The ``buf`` argument must be an object
+        supporting the buffer interface.
+
+        Raise ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read. In either
+        case, users should endeavour to resolve that situation and then
+        re-call this method. When re-calling this method users *should*
+        re-use the exact same ``buf`` object, as some backends require that
+        the exact same buffer be used.
+
+        This operation may write "short": that is, fewer bytes may be
+        written than were in the buffer.
+
+        As at any time a re-negotiation is possible, a call to ``write()``
+        can also cause read operations.
+        """
+
+        with _error_converter():
+            return self._object.write(buf)
+
+    # Get rid and do handshake ourselves?
+    def do_handshake(self) -> None:
+        """
+        Performs the TLS handshake. Also performs certificate validation
+        and hostname verification.
+        """
+
+        with _error_converter():
+            self._object.do_handshake()
+
+        # self._connection_established = True
+        # return rc
+
+    def shutdown(self) -> None:
+        """
+        Performs a clean TLS shut down. This should generally be used
+        whenever possible to signal to the remote peer that the content is
+        finished.
+        """
+
+        with _error_converter():
+            self._object.unwrap()
+
+        # self._connection_established = False
+        # return rc
+
+    def receive_from_network(self, data: bytes) -> None:
+        """
+        Receives some TLS data from the network and stores it in an
+        internal buffer.
+
+        If the internal buffer is overfull, this method will raise
+        ``WantReadError`` and store no data. At this point, the user must
+        call ``read`` or ``readinto`` to remove some data from the internal
+        buffer before repeating this call.
+        """
+
+        # TODO: This method returns a length. Can it return short? What do we
+        # do if it does?
+
+        with _error_converter():
+            written_len = self._in_bio.write(data)
+
+        assert written_len == len(data)
+
+    def peek_outgoing(self, amt: int) -> bytes:
+        """
+        Returns the next ``amt`` bytes of data that should be written to
+        the network from the outgoing data buffer, without removing it from
+        the internal buffer.
+        """
+
+        # TODO: Evaluate this for what happens when it's called with no data.
+        # What about EOF?
+        ciphertext_bytes = len(self._ciphertext_buffer)
+        if ciphertext_bytes < amt:
+            self._ciphertext_buffer += self._out_bio.read(amt - ciphertext_bytes)
+
+        return self._ciphertext_buffer[:amt]
+
+    def consume_outgoing(self, amt: int) -> None:
+        """
+        Discard the next ``amt`` bytes from the outgoing data buffer. This
+        should be used when ``amt`` bytes have been sent on the network, to
+        signal that the data no longer needs to be buffered.
+        """
+
+        del self._ciphertext_buffer[:amt]
+
+    def bytes_buffered(self) -> int:
+        """
+        Returns how many bytes are in the send buffer waiting to be sent.
+        """
+
+        return self._out_bio.pending
+
+    @property
+    def context(self) -> OpenSSLClientContext | OpenSSLServerContext:
+        """The ``Context`` object this socket is tied to."""
+
+        return self._parent_context
+
+    def cipher(self) -> CipherSuite | int | None:
+        """
+        Returns the CipherSuite entry for the cipher that has been negotiated on the connection.
+
+        If no connection has been negotiated, returns ``None``. If the cipher negotiated is not
+        defined in CipherSuite, returns the 16-bit integer representing that cipher directly.
+        """
+
+        ret = self._object.cipher()
+
+        if ret is None:
+            return None
+        else:
+            ossl_cipher, _, _ = ret
+
+        for cipher in self._ssl_context.get_ciphers():
+            if cipher["name"] == ossl_cipher:
+                break
+        # Since the cipher was negotiated using the OpenSSL context,
+        # it must exist in the list of the OpenSSL supported ciphers.
+        assert cipher["name"] == ossl_cipher
+
+        cipher_id = cipher["id"] & 0xFFFF
+        try:
+            return CipherSuite(cipher_id)
+        except ValueError:
+            return cipher_id
+
+    def negotiated_protocol(self) -> NextProtocol | bytes | None:
+        """
+        Returns the protocol that was selected during the TLS handshake.
+
+        This selection may have been made using ALPN or some future
+        negotiation mechanism.
+
+        If the negotiated protocol is one of the protocols defined in the
+        ``NextProtocol`` enum, the value from that enum will be returned.
+        Otherwise, the raw bytestring of the negotiated protocol will be
+        returned.
+
+        If ``Context.set_inner_protocols()`` was not called, if the other
+        party does not support protocol negotiation, if this socket does
+        not support any of the peer's proposed protocols, or if the
+        handshake has not happened yet, ``None`` is returned.
+        """
+
+        proto = self._object.selected_alpn_protocol()
+
+        # The standard library returns this as a str, we want bytes.
+        if proto is None:
+            return None
+
+        protoBytes = proto.encode("ascii")
+
+        try:
+            return NextProtocol(protoBytes)
+        except ValueError:
+            return protoBytes
+
+    @property
+    def negotiated_tls_version(self) -> TLSVersion | None:
+        """The version of TLS that has been negotiated on this connection."""
+
+        ossl_version = self._object.version()
+        if ossl_version is None:
+            return None
+        else:
+            return TLSVersion(ossl_version)
+
+
 class OpenSSLClientContext:
     """This class controls and creates a socket that is wrapped using the
     standard library bindings to OpenSSL to perform TLS connections on the
@@ -553,6 +858,20 @@ class OpenSSLClientContext:
             server_side=False,
             ssl_context=ossl_context,
             address=address,
+        )
+
+    def create_buffer(self, server_hostname: str) -> OpenSSLTLSBuffer:
+        """Creates a TLSBuffer that acts as an in-memory channel,
+        and contains information about the TLS exchange
+        (cipher, negotiated_protocol, negotiated_tls_version, etc.)."""
+
+        ossl_context = _init_context_client(self._configuration)
+
+        return OpenSSLTLSBuffer._create(
+            server_hostname=server_hostname,
+            parent_context=self,
+            server_side=False,
+            ssl_context=ossl_context,
         )
 
 
@@ -582,6 +901,20 @@ class OpenSSLServerContext:
             server_side=True,
             ssl_context=ossl_context,
             address=address,
+        )
+
+    def create_buffer(self) -> OpenSSLTLSBuffer:
+        """Creates a TLSBuffer that acts as an in-memory channel,
+        and contains information about the TLS exchange
+        (cipher, negotiated_protocol, negotiated_tls_version, etc.)."""
+
+        ossl_context = _init_context_server(self._configuration)
+
+        return OpenSSLTLSBuffer._create(
+            server_hostname=None,
+            parent_context=self,
+            server_side=True,
+            ssl_context=ossl_context,
         )
 
 
--- src/tlslib/tlslib.py
+++ src/tlslib/tlslib.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import os
 from abc import abstractmethod
-from collections.abc import Sequence
+from collections.abc import Buffer, Sequence
 from enum import Enum, IntEnum
 from typing import Generic, Protocol, TypeVar
 
@@ -385,6 +385,12 @@ class ClientContext(Protocol):
         (cipher, negotiated_protocol, negotiated_tls_version, etc.).
         """
 
+    @abstractmethod
+    def create_buffer(self, server_hostname: str) -> TLSBuffer:
+        """Creates a TLSBuffer that acts as an in-memory channel,
+        and contains information about the TLS exchange
+        (cipher, negotiated_protocol, negotiated_tls_version, etc.)."""
+
 
 _ClientContext = TypeVar("_ClientContext", bound=ClientContext)
 
@@ -409,14 +415,20 @@ class ServerContext(Protocol):
         (cipher, negotiated_protocol, negotiated_tls_version, etc.).
         """
 
+    @abstractmethod
+    def create_buffer(self) -> TLSBuffer:
+        """Creates a TLSBuffer that acts as an in-memory channel,
+        and contains information about the TLS exchange
+        (cipher, negotiated_protocol, negotiated_tls_version, etc.)."""
+
 
 _ServerContext = TypeVar("_ServerContext", bound=ServerContext)
 
 
 class TLSSocket(Protocol):
-    """This class implements a subtype of socket.socket that wraps
-    the underlying OS socket in an SSL context when necessary, and
-    provides read and write methods over that channel."""
+    """This class implements a socket.socket-like object that creates an OS
+    socket, wraps it in an SSL context, and provides read and write methods
+    over that channel."""
 
     @abstractmethod
     def __init__(self, *args: tuple, **kwargs: tuple) -> None:
@@ -506,6 +518,174 @@ class TLSSocket(Protocol):
     @abstractmethod
     def negotiated_tls_version(self) -> TLSVersion | None:
         """The version of TLS that has been negotiated on this connection."""
+
+
+class TLSBuffer(Protocol):
+    """This class implements an in memory-channel that creates two buffers,
+    wraps them in an SSL context, and provides read and write methods over
+    that channel."""
+
+    @abstractmethod
+    def read(self, amt: int) -> bytes:
+        """
+        Read up to ``amt`` bytes of data from the input buffer and return
+        the result as a ``bytes`` instance.
+
+        Once EOF is reached, all further calls to this method return the
+        empty byte string ``b''``.
+
+        May read "short": that is, fewer bytes may be returned than were
+        requested.
+
+        Raise ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read.
+
+        May raise ``RaggedEOF`` if the connection has been closed without a
+        graceful TLS shutdown. Whether this is an exception that should be
+        ignored or not is up to the specific application.
+
+        As at any time a re-negotiation is possible, a call to ``read()``
+        can also cause write operations.
+        """
+
+    @abstractmethod
+    def readinto(self, buffer: Buffer, amt: int) -> int:
+        """
+        Read up to ``amt`` bytes of data from the input buffer into
+        ``buffer``, which must be an object that implements the buffer
+        protocol. Returns the number of bytes read.
+
+        Once EOF is reached, all further calls to this method return the
+        empty byte string ``b''``.
+
+        Raises ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read.
+
+        May read "short": that is, fewer bytes may be read than were
+        requested.
+
+        May raise ``RaggedEOF`` if the connection has been closed without a
+        graceful TLS shutdown. Whether this is an exception that should be
+        ignored or not is up to the specific application.
+
+        As at any time a re-negotiation is possible, a call to
+        ``readinto()`` can also cause write operations.
+        """
+
+    @abstractmethod
+    def write(self, buf: Buffer) -> int:
+        """
+        Write ``buf`` in encrypted form to the output buffer and return the
+        number of bytes written. The ``buf`` argument must be an object
+        supporting the buffer interface.
+
+        Raise ``WantReadError`` or ``WantWriteError`` if there is
+        insufficient data in either the input or output buffer and the
+        operation would have caused data to be written or read. In either
+        case, users should endeavour to resolve that situation and then
+        re-call this method. When re-calling this method users *should*
+        re-use the exact same ``buf`` object, as some backends require that
+        the exact same buffer be used.
+
+        This operation may write "short": that is, fewer bytes may be
+        written than were in the buffer.
+
+        As at any time a re-negotiation is possible, a call to ``write()``
+        can also cause read operations.
+        """
+
+    @abstractmethod
+    def do_handshake(self) -> None:
+        """
+        Performs the TLS handshake. Also performs certificate validation
+        and hostname verification.
+        """
+
+    @abstractmethod
+    def cipher(self) -> CipherSuite | int | None:
+        """
+        Returns the CipherSuite entry for the cipher that has been
+        negotiated on the connection. If no connection has been negotiated,
+        returns ``None``. If the cipher negotiated is not defined in
+        CipherSuite, returns the 16-bit integer representing that cipher
+        directly.
+        """
+
+    @abstractmethod
+    def negotiated_protocol(self) -> NextProtocol | bytes | None:
+        """
+        Returns the protocol that was selected during the TLS handshake.
+        This selection may have been made using ALPN, NPN, or some future
+        negotiation mechanism.
+
+        If the negotiated protocol is one of the protocols defined in the
+        ``NextProtocol`` enum, the value from that enum will be returned.
+        Otherwise, the raw bytestring of the negotiated protocol will be
+        returned.
+
+        If ``Context.set_inner_protocols()`` was not called, if the other
+        party does not support protocol negotiation, if this socket does
+        not support any of the peer's proposed protocols, or if the
+        handshake has not happened yet, ``None`` is returned.
+        """
+
+    @property
+    @abstractmethod
+    def context(self) -> ClientContext | ServerContext:
+        """
+        The ``Context`` object this buffer is tied to.
+        """
+
+    @property
+    @abstractmethod
+    def negotiated_tls_version(self) -> TLSVersion | None:
+        """
+        The version of TLS that has been negotiated on this connection.
+        """
+
+    @abstractmethod
+    def shutdown(self) -> None:
+        """
+        Performs a clean TLS shut down. This should generally be used
+        whenever possible to signal to the remote peer that the content is
+        finished.
+        """
+
+    @abstractmethod
+    def receive_from_network(self, data: bytes) -> None:
+        """
+        Receives some TLS data from the network and stores it in an
+        internal buffer.
+
+        If the internal buffer is overfull, this method will raise
+        ``WantReadError`` and store no data. At this point, the user must
+        call ``read`` or ``readinto`` to remove some data from the internal
+        buffer before repeating this call.
+        """
+
+    @abstractmethod
+    def peek_outgoing(self, amt: int) -> bytes:
+        """
+        Returns the next ``amt`` bytes of data that should be written to
+        the network from the outgoing data buffer, without removing it from
+        the internal buffer.
+        """
+
+    @abstractmethod
+    def consume_outgoing(self, amt: int) -> None:
+        """
+        Discard the next ``amt`` bytes from the outgoing data buffer. This
+        should be used when ``amt`` bytes have been sent on the network, to
+        signal that the data no longer needs to be buffered.
+        """
+
+    @abstractmethod
+    def bytes_buffered(self) -> int:
+        """
+        Returns how many bytes are in the send buffer waiting to be sent.
+        """
 
 
 class CipherSuite(IntEnum):
