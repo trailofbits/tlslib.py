c066d14ccd345e623d2a3fdd09aba0eb961b45bb
Joop van de Pol <joop.vandepol@trailofbits.com>
2024-04-23 08:17:52 +0200

    Lint reformat

--- src/tlslib/stdlib.py
+++ src/tlslib/stdlib.py
@@ -636,13 +636,12 @@ class OpenSSLTLSBuffer:
         As at any time a re-negotiation is possible, a call to
         ``readinto()`` can also cause write operations.
         """
-        
+
         with _error_converter():
             # MyPy does not recognize:
             # - that the read function can return integers if a buffer is provided
             # - that the buffer can be something other than a bytearray
             return self._object.read(amt, buffer)  # type: ignore[return-value,arg-type]
-
 
     def write(self, buf: Buffer) -> int:
         """
