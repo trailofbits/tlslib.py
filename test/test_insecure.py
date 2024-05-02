5b54cd1fa1edba62a0074d130cab0bb0fc05aeb3
Joop van de Pol <joop.vandepol@trailofbits.com>
2024-05-02 17:46:32 +0200

    Added insecure buffers and tests

--- test/test_insecure.py
+++ test/test_insecure.py
@@ -14,10 +14,13 @@ with warnings.catch_warnings():
     from tlslib.insecure import SecurityWarning, stdlib_insecure
 
     from ._utils import (
+        handshake_buffers,
         limbo_server,
+        loop_until_success,
         retry_loop,
         tweak_client_config,
         tweak_server_config,
+        write_until_read,
     )
 
 
@@ -49,7 +52,9 @@ class TestBasic(TestInsecureBackend):
         # Overwrite client TrustStore to remove needed root certificate
         new_client_config = tweak_client_config(client_config, trust_store=None)
         with self.assertWarns(SecurityWarning):
-            insecure_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(True)
+            insecure_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(
+                True, True
+            )
 
         with server:
             with self.assertWarns(SecurityWarning):
@@ -120,3 +125,91 @@ class TestBasic(TestInsecureBackend):
                 client_sock.close()
 
                 self.assertEqual(server.server_context.insecure_configuration, insecure_config)
+
+    def test_invalid_insecure_config(self):
+        with self.assertRaises(ValueError):
+            with self.assertWarns(SecurityWarning):
+                _ = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(False, True)
+
+
+class TestBuffer(TestInsecureBackend):
+    def test_trivial_connection_buffer_insecure(self):
+        server, client_config = limbo_server("webpki::san::exact-localhost-dns-san")
+        server_config = server.server_context.configuration
+        backend = server.backend
+
+        # Overwrite client TrustStore to remove needed root certificate
+        new_client_config = tweak_client_config(client_config, trust_store=None)
+        with self.assertWarns(SecurityWarning):
+            insecure_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(
+                True, True
+            )
+
+        hostname = "localhost"
+
+        with self.assertWarns(SecurityWarning):
+            client_context = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_client_context(
+                new_client_config, insecure_config
+            )
+        server_context = backend.server_context(server_config)
+
+        with self.assertWarns(SecurityWarning):
+            client_buffer, server_buffer = handshake_buffers(
+                client_context, server_context, hostname
+            )
+
+        write_until_read(client_buffer, server_buffer, b"message 1")
+        write_until_read(server_buffer, client_buffer, b"echo: message 1")
+        write_until_read(client_buffer, server_buffer, b"message 2")
+        write_until_read(server_buffer, client_buffer, b"echo: message 2")
+
+        self.assertEqual(client_buffer.negotiated_tls_version, tlslib.TLSVersion.TLSv1_3)
+        self.assertEqual(client_buffer.cipher(), tlslib.CipherSuite.TLS_AES_256_GCM_SHA384)
+        self.assertEqual(client_buffer.negotiated_protocol(), None)
+        self.assertIsNotNone(client_buffer.getpeercert())
+        # self.assertEqual(client_buffer.getpeername(), server.socket.getsockname())
+
+        self.assertEqual(server_buffer.negotiated_tls_version, tlslib.TLSVersion.TLSv1_3)
+        self.assertEqual(server_buffer.cipher(), tlslib.CipherSuite.TLS_AES_256_GCM_SHA384)
+        self.assertEqual(server_buffer.negotiated_protocol(), None)
+        self.assertIsNone(server_buffer.getpeercert())
+
+        loop_until_success(client_buffer, server_buffer, "shutdown")
+
+    def test_create_client_buffer_insecure(self):
+        client_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.client_configuration()
+
+        for dhc, dv in ((True, True), (True, False), (False, False)):
+            with self.assertWarns(SecurityWarning):
+                insecure_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(
+                    dhc, dv
+                )
+
+            with self.assertWarns(SecurityWarning):
+                insecure_client_context = (
+                    stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_client_context(
+                        client_config, insecure_config
+                    )
+                )
+
+            with self.assertWarns(SecurityWarning):
+                _ = insecure_client_context.create_buffer("localhost")
+
+    def test_create_server_buffer_insecure(self):
+        server_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.server_configuration()
+
+        for dhc, dv in ((True, True), (True, False), (False, False)):
+            with self.assertWarns(SecurityWarning):
+                insecure_config = stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_configuration(
+                    dhc, dv
+                )
+
+            with self.assertWarns(SecurityWarning):
+                insecure_server_context = (
+                    stdlib_insecure.STDLIB_INSECURE_BACKEND.insecure_server_context(
+                        server_config, insecure_config
+                    )
+                )
+
+            with self.assertWarns(SecurityWarning):
+                _ = insecure_server_context.create_buffer()
